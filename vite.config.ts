4d7a9788f8dbdf875f8a07d3d9efcc25d50e9420
Marius Adam <marius.c.adam@gmail.com>
2025-03-19 08:46:29 +0100

    chore: update base and homepage

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/skeleton-generator",
+  base: "/skelegen",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
